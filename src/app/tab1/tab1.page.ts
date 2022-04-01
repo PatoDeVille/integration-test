7d7476393c925e2c5804e7ac733e8504a91ace75
Patricio <patomunoz.mx@gmail.com>
2022-04-01 16:40:31 +0200

    removed specs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+import { Component, OnInit } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
+import { SessionService } from '@uve/api-angular-client';
+import { OverlayService } from '@uve/shared-domain';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  constructor(private _ss: SessionService, private _os: OverlayService) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    console.debug(this._ss.getRootToken());
+  }
 
+  openModal() {
+    this._os.createOverlay(ModalComponent, (c: ModalComponent) => {
+      c.title = 'Esto es un Input';
+      c.content = 'Esto tambien es un Input';
+    });
+  }
 }
